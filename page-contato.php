b2f91714bf5f0fd83ec1e22716f0ad3b3e84dbfb
Robsonmedeirossss <robsonmedeiros125@gmail.com>
2022-09-20 19:37:21 -0300

    Add-page-contato

---
+++ page-contato.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+<main>
+    <section class="container-contato">
+        
+        <div class="contato">
+                <h1>Entre em contato com nossa equipe</h1>
+        
+                <a href="#"><img src="imagens/icone_email_branco.svg" alt="iconde de email"> email@email.com</a>
+                <a href="#"><img src="imagens/icone_telefone_branco.svg" alt="icone telefone"> (99) 9 9999-9999 (99) 9 9999-9999</a>
+                <div class="midias-sociais">
+                    <a href="#"><img src="imagens/icone_instagram_branco.svg" alt=""> @learninglab</a>
+                    <a href="facebook"> <img src="imagens/icone_facebook_branco.svg" alt="">@learninglab</a>
+                </div>
+            
+        </div>
+
+        <div class="forms">
+            <form action="" class="formulario">
+                <input type="text" placeholder="Seu melhor email para contato">
+                <input type="text" placeholder="Digite seu nome">
+                <textarea
+                      placeholder="Escreva sua mensagem, sugestão, dúvida..."
+                    ></textarea>
+                    <button class="button">Enviar mensagem</button>
+            </form>
+        </div>
+      
+
+    </section>
+</main>
+<?php get_footer(); ?>
---
+++ page-sobre.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre</title>
+    <link rel="stylesheet" type="text/css" href="estiloIndex.css">
+    <link rel="stylesheet" href="estilo-sobre.css">
+</head>
+<body>
+    <div class="cabecalho">
+
+        <header>
+        
+            <a href="file:///C:/Users/Eurilaine/Desktop/ufc%20russas/Est%C3%A1gio/vers%C3%A3o%20site/index.html#"><img src="imagens/logo.png" alt="logo"></a>
+        <nav>
+            <div class="icone-menu-mobile">
+                <span></span>
+                <span></span>
+                <span></span>
+
+            </div>
+            <ul>
+                <li><a href="page-sobre.html">Sobre</a></li>
+                <li><a href="file:///C:/Users/Eurilaine/Desktop/ufc%20russas/Est%C3%A1gio/vers%C3%A3o%20site/page-blog.html">Blog</a></li>
+                <li class="menu-curso">
+                    <a href="#">Cursos <img src="imagens/sinal-de-seta-para-baixo-para-navegar.png" alt="imagem setinha" > </a>
+                    <ul>
+                        <li><a href="#">Cursos em andamentos</a></li>
+                        <li><a href="#">Cursos presenciais</a></li>
+                        <li><a href="#">Ver todos os cursos</a></li>
+                    </ul>
+                </li>
+                <li><a href="page-contato.html">Contato</a></li>
+                <li ><a href="https://www.youtube.com/" class="botao-header">Vem fazer parte do time </a></li>
+            </ul>
+        </nav>
+
+    </header>
+    </div>
+
+<main>
+  <section class="container-quem-somos"> 
+    <div class="conteudo">
+      <h1>Quem somos</h1>
+    </div>     
+ </section>
+    <section class="container-sobre-projeto">
+      <div class="conteudo">
+        <div class="texto">
+          <h1>Sobre o projeto</h1>
+          <p>O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. </p>
+          <br>
+          <p>
+            O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui..</p>
+        </div>
+        <img src="imagens/imagem-sobre-projeto.png" alt="">
+      </div>
+    </section>
+
+    <section class="container-resultados">
+      <div class="conteudo">
+        <div class="texto">
+          <h1>Resultados</h1>
+          <p>O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. </p>
+        </div>
+        <div class="cards">
+          <div class="card">+2589 horas produzidas de conteúdo</div>
+          <div class="card">+5 cursos desenvolvidos</div>
+          <div class="card">+500 alunos beneficiados</div>
+          <div class="card">+200 horas trabalhadas</div>
+        </div>
+      </div>
+    </section>
+    <section class="container-membros">
+      <div class="conteudo">
+        <div class="texto">
+          <h1>Membros</h1>
+          <p>O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. O LearningLab nasceu durante a pandemia, imagine um texto legal aqui. </p>
+        </div>
+        <div class="cards-membros">
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+          <div class="membro">
+            <img src="imagens/imagem-membro.png" alt="">
+            <h2>Fulano Ciclano</h2>
+            <p>Engenharia de Software</p>
+            <div class="midias-sociais">
+              <a href=""><img src="imagens/icone-insta-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-face-verde.svg" alt=""></a>
+              <a href=""><img src="imagens/icone-linkedin-verde.svg" alt=""></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <section class="container-momentos">
+      <div class="conteudo">
+        <h1>Alguns Momentos</h1>
+        <div class="cards-momentos">
+          <div class="momento">
+            <img src="imagens/imagem-sobre-projeto.png" alt="">
+            <p>Aula de finalização do cuso de Figma</p>
+            <p>12/08/2022</p>
+          </div>
+          <div class="momento">
+            <img src="imagens/imagem-sobre-projeto.png" alt="">
+            <p>Aula de finalização do cuso de Figma</p>
+            <p>12/08/2022</p>
+          </div>
+          <div class="momento">
+            <img src="imagens/imagem-sobre-projeto.png" alt="">
+            <p>Aula de finalização do cuso de Figma</p>
+            <p>12/08/2022</p>
+          </div>
+        </div>
+      </div>
+    </section>
+</main>   
+</body>
+<footer>
+  <img src="imagens/logo.png" alt="imagem logo">
+  <div class="conteiner-footer">
+      
+     <div>
+          <a><h4>Sobre</h4></a>
+          <ul>
+              <li><a href="#">Nossa equipe</a></li>
+              <li><a href="#">O projeto</a></li>
+              <li><a href="#">Iniciativas</a></li>
+
+          </ul>
+          <a href="#"><img src="imagens/icone_insta.png" alt="icone instagram" class="icone-redeSocial"></a>
+          <a href="#"><img src="imagens/icone_face.png" alt="icone facebook" class="icone-redeSocial"></a>
+
+     </div> 
+     <div>
+          <a><h4>Contato</h4></a>
+          <ul >
+              <li>
+                  <img src="imagens/icone_email.png" alt="icone email">
+                  <p>email@email.com</p>
+              </li>
+              <li>
+                  <img src="imagens/icone_tel.png" alt="icone telefone">
+                  <p>(99) 99999-9999 <br>
+                  (99) 99999-9999</p>
+              </li>
+              
+          </ul>
+
+      </div> 
+      <div>
+          <a><h4>Cursos disponíveis</h4></a>
+          <ul>
+              <li><a href="#">SCRUM na prática</a></li>
+              <li><a href="#">Desenvolvimento web</a></li>
+              <li><a href="#">Git e Github</a></li>
+              <li><a href="#">Código limpo</a></li>
+              <li><a href="#">Java</a></li>
+              <li><a href="#">Figmatizando ideias: figma para iniciantes</a></li>
+
+          </ul>
+
+      </div> 
+
+  </div>
+  <p>Todos os direitos reservados. 2022.</p>
+</footer>
+
+</html>
