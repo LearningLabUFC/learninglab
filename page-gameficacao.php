8ea5d8950c262b33c1deb4b05002407784c05f47
Robsonmedeirossss <robsonmedeiros125@gmail.com>
2023-10-19 19:13:55 -0300

    pagina gameficacao estadual

--- page-gameficacao.php
+++ page-gameficacao.php
@@ -10,9 +10,9 @@
 <section class="introducao">
     <div class="conteudo">
         <div class="texto">
-            <h2 class="subtitulo">QUER GANHAR PRÊMIOS ASSISTINDO NOSSAS ATIVIDADES NA www?</h2>
+            <h2 class="subtitulo">QUER GANHAR PRÊMIOS ASSISTINDO NOSSAS ATIVIDADES NO ESTADUAL?</h2>
             <p>Olá Learnian@!</p>
-            <p>Queremos presentear você, Learnian@ fiel. Elaboramos um sistema de gameficação para premiar sua participação em nossas atividades durante o minicurso de Figma. </p>
+            <p>Queremos presentear você, Learnian@ fiel. Elaboramos um sistema de gameficação para premiar sua participação em nossas atividades durante o minicurso de Informática básica.</p>
             <p>Participe de todas as nossas atividades, para você acumular pontos e trocar por brindes, consulte a tabela.</p>
             <h2 class="subtitulo">O CARTÃO FIDELIDADE</h2>
             <p>Antes de começar você precisa resgatar nosso cartão fidelidade. Você consegue pegar o cartão na nossa primeira aula. Mas atenção, adote apenas um cartão para facilitar na sua jornada!</p>
@@ -27,9 +27,9 @@
         <ul>
             <li>Seguir o LearningLab no Instagram;</li>
             <li>Postar 1 storie e marcar o projeto;</li>
-            <li>Comentar o post do LearningLab na EEEP;</li>
-            <li>Participar do minicurso de Figma do LearningLab na EEP;</li>
-            <li>Realizar a atividade referente ao minicurso de figma;</li>
+            <li>Comentar o post do LearningLab no Estadual;</li>
+            <li>Completar a dinâmica da cruzadinha com principais conceitos da aula;</li>
+            <li>Realizar a atividade criação do currículo;</li>
             <li>Responder o formulário de feedback.</li>
             
         </ul>
@@ -52,20 +52,20 @@
     <td class="tg-9wq8" rowspan="3">1 selo #tôONLL <br>(o resgate desse selo só ocorre com<br> a acumulação de três selos rosas)</td>
   </tr>
   <tr>
-    <td class="tg-c3ow"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">Postar 3 stories relacionados ao projeto </span><br><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">LearningLab na EEEP e marcar o projeto</span></td>
+    <td class="tg-c3ow"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">Compartilhar um post do LearningLab nos stories e marcar o projeto</span><br><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">LearningLab na EEEP e marcar o projeto</span></td>
     <td class="tg-0pky">1 selo rosa</td>
   </tr>
   <tr>
-    <td class="tg-9wq8"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">Comentar o post sobre o LearningLab na EEEP</span></td>
-    <td class="tg-0pky">1 selo cores e<br>1 selo rosa<br></td>
+    <td class="tg-9wq8"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">Comentar o post sobre o LearningLab no Estadual</span></td>
+    <td class="tg-0pky">1 selo rosa<br></td>
   </tr>
   <tr>
-    <td class="tg-c3ow"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">Estar presente nas duas aulas do minicurso de Figma</span></td>
+    <td class="tg-c3ow"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">Completar a dinâmica da cruzadinha com principais conceitos da aula</span></td>
     <td class="tg-c3ow"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">1 selo amarelo</span></td>
     <td class="tg-9wq8" rowspan="3">1 selo #Aventureir@LL<br>(o resgate desse selo só ocorre <br>com a acumulação de três selos amarelos)</td>
   </tr>
   <tr>
-    <td class="tg-baqh"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">Fazer a atividade final do minicurso de figma</span></td>
+    <td class="tg-baqh"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">Fazer a atividade criação do currículo</span></td>
     <td class="tg-baqh"><span style="font-weight:400;font-style:normal;text-decoration:none;color:#000;background-color:transparent">1 selo amarelo</span></td>
   </tr>
   <tr>
@@ -85,13 +85,12 @@
         <div class="texto">
             <div class="primeira-caixa-texto">
                 <div class="subtexto">
-                    <p><strong>1 bottom personalizado do projeto</strong></p>
-                    <p><strong>1 serenata do amor</strong></p>
-                    <p><strong>1 pirulito</strong></p>
+                    <p><strong>1 kit guloseima (pipoca, bombom, pirulito)</strong></p>
+                    <p><strong>1 botton personalizado com a logo do projeto</strong></p>
                     <p><strong>1 adesivo com a logo do projeto</strong></p>
                 </div>
             </div>
-        <p>-> O resgate do prêmio será feito após a última aula do minicurso de figma.</p>
+        <p>->O resgate do prêmio será feito após a última aula do minicurso de Informática básica no Estadual.</p>
     </div>
 </section>
 
@@ -99,7 +98,7 @@
     <div class="conteudo">
         <h2 class="subtitulo">REGRAS IMPORTANTES</h2>
         <ul>
-            <li><strong>Promoção válida durante a realização do minicurso de Figma na EEEP;</strong></li>
+            <li><strong>Promoção válida durante a realização do minicurso de Informática básica no Estadual;</strong></li>
             <li>Só é permitido <strong>um resgate do prêmio por participante;</strong></li>
             <li>O projeto não se responsabiliza pela perda do cartão ou dos pontos, <strong>mantenha seu cartão fidelidade seguro;</strong></li>
         </ul>
